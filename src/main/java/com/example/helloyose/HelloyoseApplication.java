4793243425c7e6a8f71ae914cb61c22bb734c677
Barata Estevao <barataestevaomario@gmail.com>
2020-03-24 10:52:35 -0700

    first commit

---
+++ src/main/java/com/example/helloyose/HelloyoseApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloyose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloyoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloyoseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/helloyose/HelloyoseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloyose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloyoseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
